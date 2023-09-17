f35be0bbacd54228f99ae2f612f6f78bb9960309
rushi <rushikeshthange2001@gmail.com>
2023-09-17 08:43:41 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+const express=require("express")
+const cors=require("cors")
+const bodyParser = require('body-parser');
+const  route  = require("./routes/routes")
+
+
+const app=express()
+app.use(cors())
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.urlencoded({ 
+    extended: true 
+ }));
+
+app.use('/user',route)
+
+app.listen('5000',()=>{
+    console.log("server is running at 5000")
+})
+// --------------------------------------
+
+// const express=require("express")
+// const socket=require("socket.io")
+// // const cors=require("cors")
+// const app =express();
+
+// const server=app.listen(5000,()=>{
+//     console.log("server started at port 5000")
+// })
+
+// const io=socket(server,{
+//     cors:{
+//         origin:"*",
+//     }
+// })
+// io.on("connection",(socketClient)=>{
+//     console.log(socketClient.id)
+//     socketClient.on("MESSAGE",(clientdata)=>{
+//         console.log(clientdata)
+//         socketClient.emit("MESSAGE","server in sending message")
+//     })
+//     socketClient.on('BROADCAST',(clientBroadcast)=>{
+//         console.log(clientBroadcast)
+//         io.emit("sendBroadcastmsg","player 1 has won the match")
+//     })
+// })
